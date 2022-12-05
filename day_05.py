80b88d44a917b552f71e031199a3fa4015d3d26a
limelier <tudor.iacobescu.1@gmail.com>
2022-12-05 08:39:01 +0200

    Clean up day 5

--- day_05.py
+++ day_05.py
@@ -1,33 +1,41 @@
 import re
 from copy import deepcopy
 
-stacks = [list(line.strip()) for line in open("inputs/day_05_stacks", "r").readlines()]
-stacks_2 = deepcopy(stacks)
+move_line_pattern = re.compile(r"move (?P<n>\d+) from (?P<src>\d+) to (?P<dst>\d+)")
+
+stacks_1 = [
+    list(line.strip())
+    for line in open("inputs/day_05/stacks.txt", "r").readlines()
+]
+stacks_2 = deepcopy(stacks_1)
 
 
-def move(n, src, dst):
+def move_9000(n, src, dst):
     for _ in range(n):
-        stacks[dst - 1].append(stacks[src - 1].pop())
+        stacks_1[dst - 1].append(stacks_1[src - 1].pop())
 
 
 def move_9001(n, src, dst):
     moved = stacks_2[src - 1][-n:]
-    stacks_2[src - 1] = stacks_2[src - 1][:-n]
+    del(stacks_2[src - 1][-n:])
     stacks_2[dst - 1] += moved
 
 
-move_line_pattern = re.compile(r"move (?P<n>\d+) from (?P<src>\d+) to (?P<dst>\d+)")
+def tops(stacks):
+    return "".join([stack[-1] for stack in stacks])
 
-with open("inputs/day_05_moves", "r") as f:
-    for line in f.readlines():
-        matches = move_line_pattern.match(line.strip())
-        n = int(matches.group("n"))
-        src = int(matches.group("src"))
-        dst = int(matches.group("dst"))
-        move(n, src, dst)
-        move_9001(n, src, dst)
 
-tops = "".join([stack[-1] for stack in stacks])
-tops_2 = "".join([stack[-1] for stack in stacks_2])
-print(tops)
-print(tops_2)
+def main():
+    with open("inputs/day_05/moves.txt", "r") as f:
+        for line in f.readlines():
+            groups = move_line_pattern.match(line.strip()).groups()
+            n, src, dst = (int(group) for group in groups)
+            move_9000(n, src, dst)
+            move_9001(n, src, dst)
+
+    print("Part 1:", tops(stacks_1))
+    print("Part 2:", tops(stacks_2))
+
+
+if __name__ == '__main__':
+    main()
