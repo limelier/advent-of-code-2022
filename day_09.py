c7bb5cbea8043b7828493bf01dde953fe5177ad7
limelier <tudor.iacobescu.1@gmail.com>
2022-12-09 08:20:42 +0200

    Rewrite part 1 to be part-2 friendly

--- day_09.py
+++ day_09.py
@@ -11,8 +11,11 @@ class Point:
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
 
-deltas = {
+
+move_deltas = {
     "U": Point(0, -1),
     "D": Point(0, 1),
     "L": Point(-1, 0),
@@ -25,13 +28,27 @@ def chebyshev_distance(pos1: Point, pos2: Point) -> int:
     return max(abs(pos1.x - pos2.x), abs(pos1.y - pos2.y))
 
 
-def follow(tail: Point, head_old: Point, head_new: Point) -> Point:
+def clamp(num: int, lower: int, upper: int) -> int:
+    """Clamp an integer within an inclusive interval."""
+
+    if num < lower:
+        return lower
+    if num > upper:
+        return upper
+    return num
+
+
+def clamp_delta(delta: Point) -> Point:
+    """Clamp a delta to only adjacent orthogonal or diagonal movements."""
+    return Point(clamp(delta.x, -1, 1), clamp(delta.y, -1, 1))
+
+
+def follow(tail: Point, head: Point) -> Point:
     """Compute the movement of the tail and return the new position."""
 
-    # if the head has moved so that it's not touching anymore, just go where it was before
-    if chebyshev_distance(tail, head_new) > 1:
-        return head_old
-    # otherwise, there's no need to move at all
+    if chebyshev_distance(tail, head) > 1:  # not touching
+        return tail + clamp_delta(head - tail)
+
     return tail
 
 
@@ -42,13 +59,12 @@ def main():
     with open("inputs/day_09.txt") as f:
         for line in f:
             chunks = line.strip().split()
-            delta = deltas[chunks[0]]
+            delta = move_deltas[chunks[0]]
             times = int(chunks[1])
 
             for _ in range(times):
-                new_head = head + delta
-                tail = follow(tail, head, new_head)
-                head = new_head
+                head += delta
+                tail = follow(tail, head)
                 visited_by_tail.add(tail)
 
     print("Part 1:", len(visited_by_tail))
